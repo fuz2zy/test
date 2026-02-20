6c76a0cc5c66d801353640c3685b69c1de35bd2d
fuz2zy <fuzzy.garp@gmail.com>
2026-02-20 11:29:55 +0300

    /

--- core.py
+++ core.py
@@ -1,9 +1,8 @@
 from aiogram import Bot, Dispatcher
-from config import token
+from config import token, logs_path
 import logging
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(
-    "logs.log")])
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler(), logging.FileHandler(logs_path)])
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=token)
