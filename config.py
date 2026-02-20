1d8b4f3c8d60ba5ba2a628314d6e428487e80e0d
fuz2zy <fuzzy.garp@gmail.com>
2026-02-20 11:13:08 +0300

    /

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-token: str = "7500951384:AAH5j9Q6indkssSrlcz13ItH1onIw9OwJZo"
-db_path: str = "database/database.db"
-logs_path: str = "logs.log"
-admin_id: int = 7716932686
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+token = str(os.getenv("TOKEN"))
+db_path = "database/database.db"
+logs_path = "logs.log"
+admin_id = 7716932686
